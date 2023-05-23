4a1fae5dec1e9d677c2c42bac83716ebb362f62f
Pablo Vergara <vergara.pablo0616@gmail.com>
2023-05-23 18:22:11 -0400

    moved playwright to a different repo

--- tests/pw/test_saucedemo.py
+++
@@ -1,7 +0,0 @@
-from playwright.sync_api import sync_playwright, Page
-import pytest
-
-def test_title(page: Page):
-    sauceURL = 'https://www.saucedemo.com/'
-    page.goto(sauceURL)
-    assert page.title() == 'Swag Labs'
